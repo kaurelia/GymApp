ca2679aac6893bcbe9dbf35f75dfeafba5eef314
Aurelia Korbela <aurelka2000@gmail.com>
2022-02-06 21:12:51 +0100

    added library to i18next and added translations

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,11 +6,14 @@ import addEventService from "./service/addEventService";
 import deleteEventService from "./service/deleteEventService";
 import signUpEventService from "./service/signUpEventService";
 import updateEventService from "./service/updateEventService";
+import i18nextMiddleware from "i18next-http-middleware";
+import i18next from "i18next";
 
 export const database = new PrismaClient();
 export const app = express();
 const port = 5001;
 
+app.use(i18nextMiddleware.handle(i18next));
 app.use(jsonBodyParser());
 app.use(cors());
 app.post("/event", addEventService);
